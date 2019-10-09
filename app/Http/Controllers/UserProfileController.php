c1fbaa6cbacfc80d93588124f3902f50c78e0cc0
unknown <junicodefire@gmail.com>
2019-10-09 22:15:58 +0100

    edit profile done

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -27,38 +27,47 @@ class UserProfileController extends Controller
 
     }
 
-    public function role() {
-
+    public function role($id) {
+        //fetch all user base on role
     }
 
     public function update(Request $request) { 
+        $user = Auth::user();
         $this->validate($request, [
-        'firstname' => 'required|min:2',
-        'lastname' => 'required|min:2',
-        'email' => 'required|min:2|unique:users',
-        'phone_no' => 'required',
-    ]);
-    
-    if(!empty(request()->input('firstname')) && !empty(request()->input('lastname')) && !empty(request()->input('email')) && !empty(request()->input('phone_no')))
-    {
-    $user = User::findOrFail(Auth::user()->id);
-    $user->firstname = request()->input('firstname');
-    $user->email = request()->input('email');
-    $user->lastname = request()->input('lastname');
-    $user->phone_no = request()->input('phone_no');
-    $user->save();
-    
-    
-    $res['status'] = true;
-    $res['message'] = 'Your Account Was Successfully Updated.';
-    return response()->json($res, 200);
-    }
-     else
-    {
-    $res['status'] = false;
-    $res['message'] = 'An Error Occured While Trying To Update Your Account Information.';
-    return response()->json($res, 500);
-    }
+            'first_name' => 'required|min:2',
+            'last_name' => 'required|min:2',
+            'email' => 'required|min:2|unique:users,email,'.$user->id,
+            'phone' => 'required',
+        ]);
+
+        //start temporay transaction
+        DB::beginTransaction();
+        try{
+            $user->first_name = $request->input('first_name');
+            $user->last_name  = $request->input('last_name');
+            $user->email     = $request->input('email');
+            $user->phone     = $request->input('phone');
+            $user->save();
+            
+            //if operation was successful save commit save to database
+            DB::commit();
+            $res['status']  = true;
+            $res['user']    = $user;
+            $res['message'] = 'Your Account Was Successfully Updated.';
+
+            return response()->json($res, 200);
+
+        }catch(\Exception $e) {
+            //rollback what is saved
+            DB::rollBack();
+
+            $res['status'] = false;
+            $res['message'] = 'An Error Occured While Trying To Update Your Account Information.';
+            $res['hint'] = $e->getMessage();
+
+            return response()->json($res, 501);
+
+        }
    }
     
     public function destroy() {
