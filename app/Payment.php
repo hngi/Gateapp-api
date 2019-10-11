a0111b73fe2e949cb98b323d8da16636644a26be
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-10-11 14:18:20 -0700

    Resolved Justice inputs

--- app/Payment.php
+++ app/Payment.php
@@ -1,64 +1,12 @@
 <?php
 
-namespace App\Notifications;
+namespace App;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
-   
-class Payment extends Notification
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
 {
-    use Queueable;
-  
-    private $details;
-   
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
-    public function __construct($details)
-    {
-        $this->details = $details;
-    }
-   
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function via($notifiable)
-    {
-        return ['mail','database'];
-    }
-   
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->greeting($this->details['greeting'])
-                    ->line($this->details['body'])
-                    ->action($this->details['actionText'], $this->details['actionURL'])
-                    ->line($this->details['thanks']);
-    }
-  
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toDatabase($notifiable)
-    {
-        return [
-            'order_id' => $this->details['order_id']
-        ];
-    }
+    protected $fillable = [
+        'amount', 'home_id', 'id',
+    ];
 }
