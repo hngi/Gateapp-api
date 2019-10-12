ba64fc2ec0e6334f81acf2a4e3747643c8fddeb4
unknown <junicodefire@gmail.com>
2019-10-12 20:24:14 +0100

    checking 5

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,21 +9,7 @@ use App\Notifications\PaymentNotification;
   
 class PaymentController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-  
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+
     public function index()
     {
         return view('home');
