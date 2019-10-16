cdc023a79b9614f76d11c3829b890431aee9d866
Adekanye Jubril <37573577+tyroklone@users.noreply.github.com>
2019-10-16 13:06:18 -0700

    Added sms otp method

--- app/Http/Controllers/GatemanController.php
+++ app/Http/Controllers/GatemanController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Gateman;
-use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use JWTAuth;
+
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class GatemanController extends Controller
 {
@@ -19,30 +23,49 @@ class GatemanController extends Controller
     public function __construct()
     {
     	$this->user = auth()->user();
-
     }
 
 	/**
-	 * Get the residents' details, requesting for the gateman
+	 * Gets all residents request for the gateman
 	 */
     public function residentRequest()
     {
-    	$requests = Gateman::where('gateman_id', $this->user->id)->get();
+    	// ensure user has the gateman role
+        if ($this->user->role != '2') {
+	        return response()->json([
+	        	'status' => false,
+	        	'message' => 'User is not a registered gateman',
+	        ], 200);
+        }
 
-    	$users = [];
 
-    	// get the resident's name and id requesting for the gateman
-		foreach ($requests as $request) {
-	    	$user = User::select('id', 'name')->where('id', $request->user_id)->get();
+        $residents = Gateman::where('request_status', 1);
 
-			array_push($users, $user);
-		}
+        if (!$residents) {
 
-        return response()->json([
-        	'requests' => $requests->count(),
-        	'residents' => $users,
-        	'status' => true
-        ], 200);
+            $res['message'] = "Invitations not found";
+            $res['payment'] = null;
+            $res['status'] = 404;
+            return response()->json($res, $res['status']);
+        }else{
+            $stach_details = [];
+            foreach ($residents as $resident) {
+                if ((!empty($resident->user_id)) && $this->user->id == $resident->gateman_id)) {
+                    $User = User::where('id', $resident->user_id);
+                    if ($User) {
+                        // user Exist,then push detials
+                        array_push($stach_details, $User);
+                    }
+                }
+            }
+            //  returning resident users that invited current gateman user
+            $res['message'] = "Invitations from residents was found";
+            $res['resident_details'] = $stach_details;
+            $res['status'] = 200;
+
+            return response()->json($res, $res['status']);
+        }
+
 
     }
 
@@ -70,4 +93,53 @@ class GatemanController extends Controller
     {
 
     }
+    
+    /**
+     * Method to send sms otp to gateman
+     */
+    public function smsOtp($phone, $msg)
+    {
+     if(isset($phone) && isset($msg))
+     {
+      $number = User::where('phone', $phone)->get();
+      // Let me quickly check if number exists in our database
+      if(isset($number))
+      {
+       $res['status'] = true;
+      }
+       else
+      {
+       $res['status'] = false;
+       $res['data'] = "Error occured while sending OTP.";
+       return response()->json($res, 501);
+       exit();
+      }
+         
+      $curl = curl_init();
+        
+      // API token from @junicode
+         
+      curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.sms.to/sms/send",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS =>"{\n    \"message\": \"{$msg}\",\n    \"to\": \"{$number}\",\n    \"sender_id\": \"GateApp\",\n    \"callback_url\": \"https://sms.to/callback/handler\"\n}",
+      CURLOPT_HTTPHEADER => array(
+       "Authorization: Bearer s92RW6JYLZoU8BizxS8sIpRFr2TE6c9N"
+         ),
+      ));
+        
+     $response = curl_exec($curl);
+
+     curl_close($curl);
+   
+     $res['data'] = array(""otp" => $response");
+     return response()->json($res, 200);
+    }
+  }
 }
