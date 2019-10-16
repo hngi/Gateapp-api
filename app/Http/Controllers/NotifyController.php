528e7246b5543cefc271cc4d5450cb347a51bec4
davonjagah <davonkaze1@gmail.com>
2019-10-16 11:01:37 -0700

    Added an endpoint to fetch all a users notification

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Notification;
+use Illuminate\Support\Facades\DB;
 
 class NotifyController extends Controller
 {
@@ -14,4 +16,23 @@ class NotifyController extends Controller
           // Subscribe to the channel we specified in our Laravel Event
       ///    
     }
+    public function fetchnotifications($user_id)
+    {
+        $notifications = Notification::find($user_id);
+        if($notifications){
+            $res["status"] = True;
+            $res["message"] = "One User's notifications";
+            $res["data"] = $notifications;
+            return response()->json($res, 200);
+        }else{
+
+            $res["status"] = false;
+            $res["message"] = "Not found";
+            return response()->json($res, 404);
+        }
+    }
+
+
+    
+
 }
