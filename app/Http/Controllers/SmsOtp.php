eddc50af9e86ed5b84921d83d33946e1cd9b5802
Adekanye Jubril <37573577+tyroklone@users.noreply.github.com>
2019-10-17 13:30:49 +0100

    Added an exception handler

--- app/Http/Controllers/SmsOtp.php
+++ app/Http/Controllers/SmsOtp.php
@@ -13,43 +13,46 @@ class SmsOtp extends Controller
    */
   public function smsOtp($phone)
   {
-   $phone = User::where('phone', $phone)->exists();
-   // Let me quickly check if number exists in our database
-   if($phone)
-   {
-    $res['status'] = true;
-   }
-    else
-   {
-    $res['status'] = false;
-    $res['data'] = "Error occured while sending OTP.";
-    return response()->json($res, 501);
-    exit();
-   }
-         
-  $curl = curl_init();
+   try {
+        $phone = User::where('phone', $phone)->exists();
+        // Let me quickly check if number exists in our database
+        if(!$phone)
+        {
+         throw new \Exception('Phone Number Does Not Exists In Our Database.');
+        }
   
-  // API token from @junicode
+       $curl = curl_init();
+  
+       // API token from @junicode
          
-  curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.sms.to/sms/send",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS =>"{\n    \"message\": \"Gateman Messsage Here!\",\n    \"to\": \"{$phone}\",\n    \"sender_id\": \"GateApp\",\n    \"callback_url\": \"https://sms.to/callback/handler\"\n}",
-  CURLOPT_HTTPHEADER => array(
-    "Authorization: Bearer s92RW6JYLZoU8BizxS8sIpRFr2TE6c9N"
-     ),
-   ));
+       curl_setopt_array($curl, array(
+       CURLOPT_URL => "https://api.sms.to/sms/send",
+       CURLOPT_RETURNTRANSFER => true,
+       CURLOPT_ENCODING => "",
+       CURLOPT_MAXREDIRS => 10,
+       CURLOPT_TIMEOUT => 0,
+       CURLOPT_FOLLOWLOCATION => true,
+       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+       CURLOPT_CUSTOMREQUEST => "POST",
+       CURLOPT_POSTFIELDS =>"{\n    \"message\": \"Gateman Messsage Here!\",\n    \"to\": \"{$phone}\",\n    \"sender_id\": \"GateApp\",\n    \"callback_url\": \"https://sms.to/callback/handler\"\n}",
+       CURLOPT_HTTPHEADER => array(
+        "Authorization: Bearer s92RW6JYLZoU8BizxS8sIpRFr2TE6c9N"
+         ),
+      ));
    
-  $response = curl_exec($curl);
-  curl_close($curl);
-   
-  $res['data'] = array(""otp" => $response");
-  return response()->json($res, 200);
+      $response = curl_exec($curl);
+      curl_close($curl);
+
+
+      $res['status'] = true;
+      $res['data'] = array(""otp" => $response");
+      return response()->json($res, 200);
+     } catch(\Exception $e) {
+
+      $res['status'] = false;
+      $res['error_details'] = $e->getMessage();
+      $res['data'] = "Error occured while sending OTP.";
+      return response()->json($res, 501);
+     }
  }
 }
