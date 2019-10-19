5f01a6dea2ed369baf4726d60886e5cab99fd213
gabrielandy <andikan@sure.ng>
2019-10-19 16:13:15 +0100

    Updated visitor model db table

--- database/migrations/2019_10_09_002748_create_visitors_table.php
+++ database/migrations/2019_10_09_002748_create_visitors_table.php
@@ -16,17 +16,22 @@ class CreateVisitorsTable extends Migration
         Schema::create('visitors', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 50);
-            $table->date('arrival_date');
-            $table->string('car_plate_no', 20)->nullable();
+            $table->date('arrival_date')->nullable();
+            $table->string('car_plate_no', 20);
             $table->string('purpose', 40);
-            $table->string('image', 100)->default('no_image.jpg');
-            $table->string('status', 20);
-            $table->timestamp('time_in')->useCurrent();
+            $table->string('image', 100)->default('noimage.jpg');
+            $table->bigInteger('status')->default(0);
+            $table->timestamp('time_in')->nullable();
             $table->timestamp('time_out')->nullable();
             $table->unsignedBigInteger('user_id');
+            $table->string('qr_code', 10);
+            $table->unsignedBigInteger('home_id');
+            $table->string('visiting_period', 15);
+            $table->string('description');
 
             // table indexes
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('home_id')->references('id')->on('homes')->onDelete('cascade');
 
             // table meta
             $table->engine = 'InnoDB';
