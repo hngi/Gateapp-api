4aedccd7f71edc7ca0f8df7f3b94db5f5348aebc
unknown <junicodefire@gmail.com>
2019-10-20 14:06:14 +0100

    refactored

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -7,9 +7,16 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\JWTAuth;
 
 class VerificationController extends Controller
 {
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
       //generate new password for the user
     public function generatedPassword()
     {
@@ -20,7 +27,6 @@ class VerificationController extends Controller
         $myTTL = 120960; //minutes
         return $this->jwt->factory()->setTTL($myTTL);
     }
-
     public function verify(Request $request, User $user) {
 
         $this->validate($request, [
