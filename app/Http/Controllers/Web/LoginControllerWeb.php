32dd2abba182e48edd60794b97cf1629832284b5
Tobbhie <tobifolajin@gmail.com>
2019-10-23 21:08:02 +0100

    Initial Commit for backend mixed with frontend

---
+++ app/Http/Controllers/Web/LoginControllerWeb.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\JWTAuth;
+use Carbon\Carbon;
+use App\User;
+
+class LoginController extends Controller
+{
+     /**
+     * @var \Tymon\JWTAuth\JWTAuth
+     */
+    protected $jwt;
+
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
+    public function expireTime() {
+        $myTTL = 120960; //minutes
+        return $this->jwt->factory()->setTTL($myTTL);
+    }
+
+    public function authenticate(Request $request)
+    {     
+        $this->expireTime();
+        // Do a validation for the input
+        $this->validateRequest($request);
+        $credentials = User::where('phone', $request->input('phone'))
+                             ->where('device_id', $request->input('device_id'))->first();
+        if(!$credentials){
+            return response()->json(['message_1' => 'invalid_credentials',
+             'message_2' => 'Note: device type or phone number is not recognize, verify account and make this device your registered device'], 404);    
+        }
+
+        if (!$token = Auth::guard()->login($credentials)) {
+            return response()->json(['message_1' => 'invalid_credentials',
+             'message_2' => 'Note: device type or phone number is not recognize, verify account and make this device your registered device'], 404);
+        }
+
+        $image_link = 'https://res.cloudinary.com/getfiledata/image/upload/';
+        $image_format = 'w_200,c_thumb,ar_4:4,g_face/';
+        
+        $user = Auth::guard('api')->user();
+        if ($user->email_verified_at != null) {
+            $msg['success'] = true;
+            $msg['message'] = 'Login Successful!';
+            $msg['user'] = $user;
+            $msg['image_link'] = $image_link;
+            $msg['image_small_view_format'] = $image_format;
+            $msg['token'] = 'Bearer '. $token;
+            $msg['token_type'] = 'bearer';
+            $msg['expires_in(minutes)'] = auth()->factory()->getTTL();
+            return response()->json($msg, 200);
+        } else {
+            $msg['success'] = false;
+            $msg['message'] = 'Login Unsuccessful: account has not been confirmed yet!';
+            return response()->json($msg, 401);
+        }
+    }
+
+    public function refresh()
+    {   
+        return response()->json([
+            'access_token' => 'Bearer '. auth()->refresh(),
+            'token_type'   => 'bearer',
+            'expires_in(minutes)'   => (int)auth()->factory()->getTTL()
+        ], 200);
+    }
+
+    public function validateRequest(Request $request){
+            $rules = [
+                'phone' => 'required',
+            ];
+            $messages = [
+                'phone' => ':attribute is required',
+            ];
+        $this->validate($request, $rules, $messages);
+    }
+}
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light our-nav col-12 border-bottom">
+    <a class="navbar-brand app-logo" href="#">
+        <img src="images/Logo.png" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse"
+        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse nav-utilities" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-1">
+                <a class="nav-link" href="#">Products</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a class="nav-link" href="#">Partners</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item mx-1">
+                <a class="nav-link" href="#">Contact Us</a>
+            </li>
+
+        </ul>
+        <button class="btn btn-success login-btn mx-2">LOGIN</button>
+        <button class="btn btn-outline-success signup-btn mx-2">SIGN UP</button>
+    </div>
+</nav>
