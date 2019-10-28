6f12eb043f2c54b0affe8283b3f87fe14644a3aa
oluwamayowaf <e_fadairo@yahoo.com>
2019-10-28 15:44:24 +0100

    Get all service providers by estate

--- app/Service_Provider.php
+++ app/Service_Provider.php
@@ -11,4 +11,10 @@ class Service_Provider extends Model
     protected $fillable = [
         'name', 'phone', 'description', 'image', 'estate_id', 'category_id'
     ];
+
+    public function category()
+    {
+       
+        return $this->belongsTo('App\Category');
+    }
 }
