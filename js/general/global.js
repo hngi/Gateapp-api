7ba1a223adabb342259e4806047eac58261dea5e
unknown <junicodefire@gmail.com>
2019-10-28 15:45:53 +0100

    directory structuring

---
+++ js/general/global.js
@@ -0,0 +1,5 @@
+/*
+Import Note: The global.js hold every general data that needs that is to run in the app
+for example: If the app needs to redirect to login after session has expires
+             or another example where token or user data ae destroyed from the broswer
+*/
---
+++ js/general/placeholder.js
@@ -0,0 +1,8 @@
+/*
+Import Note: The placeholder.js hold every repeated data that needs to be appllied to more than 3 pages 
+for example-> The website title or name should be place in the placholder
+
+*/
+//Website Title holder
+const website_name = '';
+const website_logo = '';
