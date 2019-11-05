81a51844fff7bc988bcccf6045fba20f030df159
unknown <junicodefire@gmail.com>
2019-11-05 17:50:44 +0100

    hurray

--- js/general/routes.js
+++ js/general/routes.js
@@ -81,36 +81,7 @@ class Routes {
     get serviceProviderSubmit() {
         return `api/v1/service_provider/create_request`;
     }
+    path(path) {
+        return path; 
+    }
 }
-
-// //Authentication Paths
-// const signin = 'api/v1/login/admin';
-// //Dashboard Statistic
-
-// //Super Admin - Admin Page 
-// //Weeekly
-// const weeklyEstate = `api/v1/statistics/weeklyEstate`;
-// const weeklyServiceProvider = `api/v1/statistics/weeklyService`;
-// const weeklyServiceVisits = `api/v1/statistics/weeklyVisits`;
-// //Monthly
-// const monthlyEstate = `api/v1/statistics/monthlyEstate`;
-// const monthlyServiceProvider = `api/v1/statistics/monthlyService`;
-// const monthlyServiceVisits = `api/v1/statistics/monthlyVisits`;
-// //All 
-// const allEstate = `api/v1/statistics/estate`;
-// const allServiceProvider = `api/v1/statistics/service`;
-// const allVisits = `api/v1/statistics/visits`;
-// // Get All Estate Admin Information 
-// const fetchAllEstateAdmin = `api/v1/admin`;
-
-// const revokeEstateAdminAccess = `api/v1/revokeadminaccess`;
-// const resetEstateAdminPassword = `api/v1/unrevokeadminaccess`;
-
-// //Estates
-// const addEstate = `api/v1/estate`;
-// const editEstate = `api/v1/estate/${estate_id}`;//if you want to create something like this declare the variblea globaly at placeholder.js script file and pass whatwver data to it 
-
-// //Newsletter Subscribe
-// const newsletter = `api/v1/newsletter`;
-// //Service Providers Submit Request on Website
-// const serviceProviderSubmit = `api/v1/service_provider/create_request`;
