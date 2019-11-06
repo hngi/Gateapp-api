1725f8bcb9a9b3a652c2128a3a8fe8143852e64f
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2019-11-06 16:48:11 +0100

    fixed ui

--- js/general/third_party_cdn.js
+++ js/general/third_party_cdn.js
@@ -1,23 +1,32 @@
-
 /* Import all you external cdn here and refer this third party file to the page
 Please note the order of arrangement plays an important role 
 */
 /**********************************Jquery CDN ********************************************************/
-document.write(`<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>`);
+document.write(
+  `<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>`
+);
 
 /**********************************Sweet Alert CDN ********************************************************/
-document.write(`<script type='text/javascript' src="https://cdn.jsdelivr.net/npm/sweetalert2" aria-hidden="true"></script>`);
+document.write(
+  `<script type='text/javascript' src="https://cdn.jsdelivr.net/npm/sweetalert2" aria-hidden="true"></script>`
+);
 
 /**********************************Propper.js CDN ********************************************************/
-document.write(`<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>`);
+document.write(
+  `<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>`
+);
 
 /**********************************Boostrap CDN ********************************************************/
-document.write(`<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>`);
+document.write(
+  `<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>`
+);
 
 /**********************************Slick.js CDN ********************************************************/
-document.write(`<script src="slick/slick.js"></script>`);
+document.write(`<script src="../slick/slick.js"></script>`);
 
 //*********************************Axios****************************************************************//
-document.write(`<script src="https://unpkg.com/axios/dist/axios.min.js"></script>`);
+document.write(
+  `<script src="https://unpkg.com/axios/dist/axios.min.js"></script>`
+);
 
 console.clear();
