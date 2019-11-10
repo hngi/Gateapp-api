bbd8a3e5cd4ba147bfed95d6c8994d9766f3111a
unknown <junicodefire@gmail.com>
2019-11-10 08:41:45 +0100

    made super-admin command to create in the database

---
+++ app/Console/Commands/SuperAdmin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+
+class SuperAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'super-admin:create {name : Name of the super admin} {email : Email of the super admin} {password : create secure password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command creates a super admin in the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $email = $this->argument('email');
+        $password = Hash::make($this->argument('password'));
+
+        try{
+            DB::table('users')->insert(
+                ['name' => $name, 'email' => $email, 'password' => $password]
+            );    
+
+            $this->info('Admin Created Successfully');
+        }catch(\Exception $e) {
+            $this->info('An Error occured: '. $e->getMessage());
+        }
+    }
+}
