b52d6ef8c2d4e805014c619a51b422b70ae075eb
unknown <junicodefire@gmail.com>
2019-11-10 16:20:12 +0100

    refactoring

--- js/general/global.js
+++ js/general/global.js
@@ -3,3 +3,12 @@ Import Note: The global.js hold every general data that needs that is to run in 
 for example: If the app needs to redirect to login after session has expires
              or another example where token or user data ae destroyed from the broswer
 */
+const logout = document.querySelector('[data-admin-logout]');
+
+const logoutAdmin = () => {
+    localStorage.removeItem('gateguard-admin');
+    location.replace('../login.html');
+}
+
+
+logout.addEventListener('click', logoutAdmin);
