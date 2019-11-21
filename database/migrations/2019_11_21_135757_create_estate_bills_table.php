568a6eca1c1a135332e4ba1345cdb70bc0e14ee3
Andikan Gabriel <andikan@sure.ng>
2019-11-21 15:06:30 +0100

    Added estate bills schema

---
+++ database/migrations/2019_11_21_135757_create_estate_bills_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estate_bills', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('estate_id');
+            $table->string('item');
+            $table->string('icon_link');
+            $table->float('base_amount');
+            $table->timestamps();
+
+            $table->foreign('estate_id')->references('id')->on('estates')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estate_bills');
+    }
+}
